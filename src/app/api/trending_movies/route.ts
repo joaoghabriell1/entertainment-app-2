4c07da3071e2bb42cc5e5af31ee7b15e05f856da
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-09-21 02:34:41 -0300

    .

--- src/app/api/trending_movies/route.ts
+++ src/app/api/trending_movies/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server";
 
-const API_TOKEN: string =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDczOWMxYTcxMDU5OTIxMWVjNDAwODdjOGNlNWU0OCIsInN1YiI6IjY1MGI2MzdmYWVkZTU5MWFiMzM4OTMyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5tR_nnHxt5WCM86ST6Zw8XYezk-of-K0Bt3AbwfgkpY";
-
 export async function GET() {
-  const API_URL = "https://api.themoviedb.org/3/trending/movie/day";
-  const res = await fetch(`${API_URL}`, {
+  const res = await fetch("https://api.themoviedb.org/3/trending/movie/day", {
     headers: {
       "Content-type": "Application-json",
-      Authorization: API_TOKEN,
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDczOWMxYTcxMDU5OTIxMWVjNDAwODdjOGNlNWU0OCIsInN1YiI6IjY1MGI2MzdmYWVkZTU5MWFiMzM4OTMyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5tR_nnHxt5WCM86ST6Zw8XYezk-of-K0Bt3AbwfgkpY",
     },
   });
   const data = await res.json();
